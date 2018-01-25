44ef25d8da8984d2e27709236501ee9e2b05d3b8
DESKTOP-45BGSFN\bbang <bbangwon.chung@gmail.com>
2018-01-26 00:55:14 +0900

    자마린 스터디 소스 올림

---
+++ Greetings/Greetings/Greetings/GreetingsPage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Greetings
+{
+	public class GreetingsPage : ContentPage
+	{
+		public GreetingsPage ()
+		{
+            Content = new Label
+            {
+                Text = "인사말, Xamarin Forms",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                FontSize = Device.GetNamedSize(NamedSize.Default, typeof(Label)),
+                FontAttributes = FontAttributes.Bold | FontAttributes.Italic
+            };
+		}
+	}
+}
---
+++ Hello/Hello/Hello/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Hello
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children =
+                    {
+                        new Label
+                        {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "환영합니다. Xamarin Forms"
+                        }
+                    }
+                }
+            };
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
