863478e8b9d4c860e325bd579341d516de496d4c
DESKTOP-45BGSFN\bbang <bbangwon.chung@gmail.com>
2018-06-14 17:47:04 +0900

    396p 까지 공부

---
+++ TheInteractiveInterface/TheInteractiveInterface/TheInteractiveInterface/SliderDemoPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TheInteractiveInterface
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SliderDemoPage : ContentPage
+	{
+		public SliderDemoPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            Slider slider = (Slider)sender;
+            label.Text = String.Format("Slider = {0:F2}", slider.Value);
+        }
+    }
+}
