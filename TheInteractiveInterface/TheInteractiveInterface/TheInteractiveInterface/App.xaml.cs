ea1b4e9446049c64a90f96de3491f7e7fa2481bd
breadone <bbangwon.chung@gamil.com>
2018-06-17 16:07:54 +0900

    405p 까지~

--- TheInteractiveInterface/TheInteractiveInterface/TheInteractiveInterface/App.xaml.cs
+++ TheInteractiveInterface/TheInteractiveInterface/TheInteractiveInterface/App.xaml.cs
@@ -11,7 +11,7 @@ namespace TheInteractiveInterface
 		{
 			InitializeComponent();
 
-			MainPage = new TextFadePage();
+			MainPage = new StepperDemoPage();
 		}
 
 		protected override void OnStart ()
---
+++ TheInteractiveInterface/TheInteractiveInterface/TheInteractiveInterface/StepperDemoPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TheInteractiveInterface
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class StepperDemoPage : ContentPage
+	{
+		public StepperDemoPage ()
+		{
+			InitializeComponent ();
+
+            stepper_ValueChanged(stepper, null);
+		}
+
+        private void stepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            Stepper stepper = (Stepper)sender;
+            button.BorderWidth = stepper.Value;
+            label.Text = stepper.Value.ToString("F0");
+        }
+    }
+}
