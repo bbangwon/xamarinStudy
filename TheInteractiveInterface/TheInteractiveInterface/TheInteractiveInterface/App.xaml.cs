bbff40fb636cfe2556c7cf2bbe31826fb2254509
breadone <bbangwon.chung@gamil.com>
2018-06-24 16:55:23 +0900

    435p 까지

--- TheInteractiveInterface/TheInteractiveInterface/TheInteractiveInterface/App.xaml.cs
+++ TheInteractiveInterface/TheInteractiveInterface/TheInteractiveInterface/App.xaml.cs
@@ -11,7 +11,7 @@ namespace TheInteractiveInterface
 		{
 			InitializeComponent();
 
-            MainPage = new SearchBarDemoPage();
+            MainPage = new DaysBetweenDatesPage();
 		}
 
 		protected override void OnStart ()
---
+++ TheInteractiveInterface/TheInteractiveInterface/TheInteractiveInterface/DaysBetweenDatesPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TheInteractiveInterface
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DaysBetweenDatesPage : ContentPage
+	{
+		public DaysBetweenDatesPage ()
+		{
+			InitializeComponent ();
+
+            OnDatePicker_DateSelected(null, null);
+		}
+
+        private void OnDatePicker_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            int days = (toDatePicker.Date - fromDatePicker.Date).Days;
+            resultLabel.Text = String.Format("{0} day{1} between dates", days, days == 1 ? "" : "s");
+        }
+    }
+}
