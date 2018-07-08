70cb58bf3e3fa3158fc3262c8473a7df09982f4d
DESKTOP-45BGSFN\bbang <bbangwon.chung@gmail.com>
2018-07-09 00:54:24 +0900

    488p

--- MasteringTheGrid/MasteringTheGrid/MasteringTheGrid/App.xaml.cs
+++ MasteringTheGrid/MasteringTheGrid/MasteringTheGrid/App.xaml.cs
@@ -11,7 +11,7 @@ namespace MasteringTheGrid
 		{
 			InitializeComponent();
 
-			MainPage = new SimpleGridDemoPage();
+			MainPage = new GridCodeDemoPage();
 		}
 
 		protected override void OnStart ()
