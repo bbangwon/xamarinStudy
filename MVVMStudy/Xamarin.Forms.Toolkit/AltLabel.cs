ad4e6e63c3f3d33df18886977f01901bbcfa7358
breadone <bbangwon.chung@gamil.com>
2018-07-17 12:05:51 +0900

    521p

---
+++ MVVMStudy/Xamarin.Forms.Toolkit/AltLabel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xamarin.Forms.Toolkit
+{
+    public class AltLabel : Label
+    {
+        public static readonly BindableProperty PointSizeProperty =
+            BindableProperty.Create("PointSize", typeof(double), typeof(AltLabel), 8.0, propertyChanged: OnPointSizeChanged);
+
+        public AltLabel()
+        {
+            SetLabelFontSize((double)PointSizeProperty.DefaultValue);
+        }
+
+        public double PointSize
+        {
+            set { SetValue(PointSizeProperty, value); }
+            get { return (double)GetValue(PointSizeProperty); }
+        }
+
+        static void OnPointSizeChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            ((AltLabel)bindable).OnPointSizeChanged((double)oldValue, (double)newValue);
+        }
+
+        void OnPointSizeChanged(double oldValue, double newValue)
+        {
+            SetLabelFontSize(newValue);
+        }
+
+        void SetLabelFontSize(double pointSize)
+        {
+            FontSize = 160 * pointSize / 72;
+        }
+    }
+}
