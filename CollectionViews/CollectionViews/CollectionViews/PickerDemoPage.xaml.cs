f1f467cd88b737243a9ca1a763cdceb8eb4324a1
DESKTOP-45BGSFN\bbang <bbangwon.chung@gmail.com>
2018-07-23 02:08:23 +0900

    561p

---
+++ CollectionViews/CollectionViews/CollectionViews/PickerDemoPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CollectionViews
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PickerDemoPage : ContentPage
+	{
+		public PickerDemoPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (entry == null)
+                return;
+
+            Picker picker = (Picker)sender;
+            int selectedIndex = picker.SelectedIndex;
+
+            if (selectedIndex == -1)
+                return;
+
+            string selectedItem = picker.Items[selectedIndex];
+            PropertyInfo propertyInfo = typeof(Keyboard).GetRuntimeProperty(selectedItem);
+            entry.Keyboard = (Keyboard)propertyInfo.GetValue(null);
+
+        }
+    }
+}
