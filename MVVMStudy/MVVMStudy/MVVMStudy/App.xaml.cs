ff1b04757e3bada0b1d60ae875864286863ed98b
DESKTOP-45BGSFN\bbang <bbangwon.chung@gmail.com>
2018-07-22 15:44:56 +0900

    544p

--- MVVMStudy/MVVMStudy/MVVMStudy/App.xaml.cs
+++ MVVMStudy/MVVMStudy/MVVMStudy/App.xaml.cs
@@ -11,7 +11,7 @@ namespace MVVMStudy
 		{
 			InitializeComponent();
 
-			MainPage = new HslSlidersPage();            
+			MainPage = new PowersOfThreePage();            
 		}
 
 		protected override void OnStart ()
