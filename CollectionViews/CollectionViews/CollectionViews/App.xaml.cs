556eacad64886e8a4437ac91d4ef3fddd889d26d
breadone <bbangwon.chung@gamil.com>
2018-07-24 00:00:59 +0900

    564p

--- CollectionViews/CollectionViews/CollectionViews/App.xaml.cs
+++ CollectionViews/CollectionViews/CollectionViews/App.xaml.cs
@@ -11,7 +11,7 @@ namespace CollectionViews
 		{
 			InitializeComponent();
 
-			MainPage = new PickerDemoPage();
+			MainPage = new PickerBindingPage();
 		}
 
 		protected override void OnStart ()
