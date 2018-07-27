9f265de34bf2ad951542041d01a433c9535d2c9e
breadone <bbangwon.chung@gamil.com>
2018-07-27 15:29:21 +0900

    572p

--- CollectionViews/CollectionViews/CollectionViews/App.xaml.cs
+++ CollectionViews/CollectionViews/CollectionViews/App.xaml.cs
@@ -11,7 +11,7 @@ namespace CollectionViews
 		{
 			InitializeComponent();
 
-			MainPage = new ListViewListPage();
+			MainPage = new ListViewArrayPage();
 		}
 
 		protected override void OnStart ()
---
+++ CollectionViews/CollectionViews/CollectionViews/ListViewArrayPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CollectionViews
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ListViewArrayPage : ContentPage
+	{
+		public ListViewArrayPage ()
+		{
+			InitializeComponent ();
+
+            Array.Sort<Color>((Color[])listView.ItemsSource, (Color color1, Color color2) =>
+            {
+                if (color1.Hue == color2.Hue)
+                    return Math.Sign(color1.Luminosity - color2.Luminosity);
+
+                return Math.Sign(color1.Hue - color2.Hue);
+
+            });
+		}
+	}
+}
