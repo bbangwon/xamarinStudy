ab7b7175b8a8ed2553426b7c7495dff1ff2f9b93
breadone <bbangwon.chung@gamil.com>
2018-07-27 14:26:07 +0900

    568p

---
+++ CollectionViews/CollectionViews/CollectionViews/ListViewListPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CollectionViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ListViewListPage : ContentPage
+	{
+		public ListViewListPage ()
+		{
+			InitializeComponent ();
+
+            var lColors = new List<Color>
+            {
+                Color.Aqua, Color.Black, Color.Blue, Color.Fuchsia,
+                Color.Gray, Color.Green, Color.Lime, Color.Maroon,
+                Color.Navy, Color.Olive, Color.Pink, Color.Purple,
+                Color.Red, Color.Silver, Color.Teal, Color.White, Color.Yellow
+            };
+
+            listView.ItemsSource = lColors.Select(_ => _.ToString());
+        }
+	}
+}
