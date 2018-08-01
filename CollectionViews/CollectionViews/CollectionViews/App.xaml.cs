70b03ea1c877ae54c9fcd9201d98b274ec1e5f64
breadone <bbangwon.chung@gamil.com>
2018-08-01 11:35:45 +0900

    574p

--- CollectionViews/CollectionViews/CollectionViews/App.xaml.cs
+++ CollectionViews/CollectionViews/CollectionViews/App.xaml.cs
@@ -11,7 +11,7 @@ namespace CollectionViews
 		{
 			InitializeComponent();
 
-			MainPage = new ListViewArrayPage();
+			MainPage = new ListViewLoggerPage();
 		}
 
 		protected override void OnStart ()
---
+++ CollectionViews/CollectionViews/CollectionViews/ListViewLoggerPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CollectionViews
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ListViewLoggerPage : ContentPage
+	{
+		public ListViewLoggerPage ()
+		{
+			InitializeComponent ();
+
+            ObservableCollection<DateTime> list = new ObservableCollection<DateTime>();
+            listView.ItemsSource = list;
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                list.Add(DateTime.Now);
+                return true;
+            });
+
+		}
+	}
+}
