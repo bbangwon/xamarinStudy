83cd4933256dee9bffa275e90b902c2c28c006dd
bread.one <bbangwon.chung@gmail.com>
2018-10-10 08:57:24 +0900

    708p

---
+++ AsyncAndFileIO/AsyncAndFileIO/AsyncAndFileIO/BitmapInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AsyncAndFileIO
+{
+    class BitmapInfo
+    {
+        public BitmapInfo(int pixelWidth, int pixelHeight, int[] iterationCounts)
+        {
+            PixelWidth = pixelWidth;
+            PixelHeight = pixelHeight;
+            IterationCounts = iterationCounts;
+        }
+
+        public int PixelWidth { get; private set; }
+        public int PixelHeight { get; private set; }
+        public int[] IterationCounts { get; private set; }
+    }
+}
