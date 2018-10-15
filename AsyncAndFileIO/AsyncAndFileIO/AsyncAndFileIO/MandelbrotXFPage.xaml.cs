f446560e02a4015d11a95bedf7b0381ebb94a603
bread.one <bbangwon.chung@gmail.com>
2018-10-15 10:03:31 +0900

    716p

---
+++ AsyncAndFileIO/AsyncAndFileIO/AsyncAndFileIO/MandelbrotXFPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AsyncAndFileIO
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MandelbrotXFPage : ContentPage
+    {
+        public MandelbrotXFPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPageSizeChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AsyncAndFileIO/Xamarin.Forms.Toolkit/BooleanNegationConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Xamarin.Forms.Toolkit
+{
+    public class BooleanNegationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+    }
+}
