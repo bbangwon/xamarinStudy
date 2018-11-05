587914dcf75bc3ae96cb3b315ba8de74c3c5c95e
bread.one <bbangwon.chung@gmail.com>
2018-11-05 09:44:51 +0900

    748p

--- Transforms/Transforms/Transforms/App.xaml.cs
+++ Transforms/Transforms/Transforms/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Transforms
         {
             InitializeComponent();
 
-            MainPage = new AnchoredScaleDemoPage();
+            MainPage = new PlaneRotationDemoPage();
         }
 
         protected override void OnStart()
