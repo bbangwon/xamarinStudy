f057f5e95611f49b6d5eafbc529ddfa39a3a8744
bbangwon <bbangwon.chung@gmail.com>
2018-11-22 22:15:18 +0900

    771p

---
+++ Animation/Animation/Animation/AnimationTryoutPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Animation
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AnimationTryoutPage : ContentPage
+	{
+		public AnimationTryoutPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            button.RotateTo(360);
+        }
+    }
+}
